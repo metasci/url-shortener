c37185bddc7617eb346886356b13a2c55e8cc3e8
Nick Beaudoin <believewithyoureyes@gmail.com>
2016-06-12 19:01:30 +0000

    h

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ var express = require('express');
 var routes = require('./app/index.js');
 var api = require('./app/shortener.js');
 var mongo = require('mongodb').MongoClient;
-var path = require('path');
 
 var app = express();
 
 app.use('/client', express.static(process.cwd() + '/client'));
 var url = 'mongodb://localhost:27017/myDb';
 
-mongo.connect(process.env.MONGOLAB_URI || url, function(err, db){
+var uri = "mongodb://nick:url@ds013584.mlab.com:13584/heroku_rstnv79m";
+
+mongo.connect(uri || url, function(err, db){
   
   if(err) throw 'Database failed to connect';
   
-  app.set('client', path.join(__dirname + 'client'));
   
   
   
